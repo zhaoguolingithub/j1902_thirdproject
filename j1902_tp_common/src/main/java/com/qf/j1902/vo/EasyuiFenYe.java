b77c9083d50b4737dfa045b63e3b90c5558ec671
qianyunyang <2997272453@qq.com>
2019-07-10 11:30:34 +0800

    qianyunyang1

---
+++ j1902_tp_common/src/main/java/com/qf/j1902/vo/EasyuiFenYe.java
@@ -0,0 +1,14 @@
+package com.qf.j1902.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2019/7/5.
+ */
+@Data
+public class EasyuiFenYe   {
+    private long total;
+    private List<?> rows;
+}
---
+++ j1902_tp_common/src/main/java/com/qf/j1902/vo/MsgResult.java
@@ -0,0 +1,12 @@
+package com.qf.j1902.vo;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2019/7/4.
+ */
+@Data
+public class MsgResult {
+    private int status;
+    private String msg;
+}
